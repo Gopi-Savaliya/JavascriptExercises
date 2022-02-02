22c06d24c9a5d34222b9edf1bf242610554895d9
Gopi Savaliya <gopis@Gopis-MacBook-Air.local>
2022-02-02 15:10:11 +0530

    Isssue Resolved

--- Exercises-1 to 5/ex3.js
+++ Exercises-1 to 5/ex3.js
@@ -1,10 +1,4 @@
-function Sum(n) {
-    let sum = 0;
-    for(let i = 1;i <= n; i++) {
-        sum += i;
-    }
-    return sum;
-}
+let Sum = (n) => (parseInt(n) * (parseInt(n) + 1)) / 2;
 
 let n = prompt("Enter n for sum(exercise):",0); 
 document.getElementById("ex3").innerHTML = `Sum of the numbers 1 to ${n}: ` + Sum(n);
